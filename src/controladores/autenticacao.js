fb6dd7d42af4d420c7da1b2cde9d4e10dce36073
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2024-05-21 11:49:07 -0300

    Update autenticacao.js

--- src/controladores/autenticacao.js
+++ src/controladores/autenticacao.js
@@ -28,8 +28,7 @@ const login = async (req, res) => {
   
   
       return res.status(200).json({
-        usuario: dadosUsuario,
-        token: tokenGerado
+        usuario: dadosUsuario
       });
     } catch (error) {
       return res.status(500).json({ mensagem: "Erro interno do servidor" })
